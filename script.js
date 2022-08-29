f382e6fa886eae83539dbc38b88251e5623a4445
Álvaro Arias Rivero <alvaroariasrivero92@gmail.com>
2022-08-29 19:25:53 +0200

    add scroll functions

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
 const open = document.getElementById('open');
 const close = document.getElementById('close');
+const container = document.querySelector('.container');
 
-const container = document.querySelector('.container')
+let scrollTop;
+let scrollLeft;
 
-open.addEventListener('click', () => container.classList.add('show-nav'))
+open.addEventListener('click', () =>{
+    container.classList.add('show-nav');
+    scrollTop = window.pageYOffset;
+    scrollLeft = window.pageXOffset
+    window.scrollTo({top: 0});
+    window.onscroll = function() {
+        window.scrollTo(0, 0);
+    };
+});
 
-close.addEventListener('click', () => container.classList.remove('show-nav'))
+close.addEventListener('click', () => {
+    container.classList.remove('show-nav');
+    window.onscroll = function() {};
+    window.scrollTo({top: scrollTop, left: scrollLeft, behavior:"smooth"});
+});
